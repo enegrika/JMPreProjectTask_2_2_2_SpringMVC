0b93cb3d73b05ec1722570b7f9d3388fbafd1845
enegrika <sergeigroshev@gmail.com>
2021-05-26 08:13:21 +0700

    add Car Controller

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -2,6 +2,11 @@ package web.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+// в приложении нет метода main,
+// его запуск происходит из-под Tomcat и для этого требуется отдельный класс AppInit,
+// который ссылается на корневой конфигурационный файл и обозначает,
+// на каком url будет находиться наше приложение.
+
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     // Метод, указывающий на класс конфигурации
